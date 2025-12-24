5372eab439ca79ebb0a720875ac521094cf14900
Stepan Khachatryan <stepan.v.khachatryan@gmail.com>
2025-12-25 01:44:25 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-
+// 1. Fetch the JSON data
+fetch('data/floods.json')
+    .then(response => response.json())
+    .then(data => {
+        // 2. Send data to Map Module to draw pins
+        initMapMarkers(data); 
+        
+        // 3. Send data to Filter Module to setup slider limits
+        initTimeSlider(data);
+    });
