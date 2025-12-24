8d1e4bfe60b62e9919a55d8b8c78608a7db2f514
Stepan Khachatryan <stepan.v.khachatryan@gmail.com>
2025-12-25 01:44:39 +0400

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,1 +1,17 @@
+var map = L.map('map-container').setView([40.0691, 45.0382], 8);
+var markersLayer = L.layerGroup().addTo(map);
 
+function initMapMarkers(data) {
+    data.forEach(item => {
+        // Logic to choose icon color based on severity
+        var marker = L.marker([item.lat, item.lng]);
+        
+        // Logic to build the iframe popup
+        marker.bindPopup(buildPopupHTML(item));
+        
+        // Add date metadata to the object for filtering later
+        marker.options.date = new Date(item.date);
+        
+        markersLayer.addLayer(marker);
+    });
+}
